e525a788b3dbd5d91f7d187dd1cfc4cfd928c059
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-01 20:52:11 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,7 @@ from setup import *
 
 eval(10, 200, 15, 30)
 
-# this function call tells the bot to do the following: look at the first 10 submissions in https://www.reddit.com/r/acturnips/new/. If a submission (1) advertises a buying price over 200 bells and (2) was posted less than 30 minutes ago, but more than 15 minutes ago then reply to the submission.
+# this function call tells the bot to do the following: 
+# look at the first 10 submissions on https://www.reddit.com/r/acturnips/new/. 
+# If a submission advertises a buying price over 200 bells and
+# was posted less than 30 minutes ago, but more than 15 minutes ago then reply to the submission.
