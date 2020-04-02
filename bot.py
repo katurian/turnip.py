95f9ef5b3aa38e282757d91ee6ee1cbc0e4f5a19
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-01 20:50:43 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,5 @@
-import praw
-import time
-from time import sleep
-import random
-import re
+from setup import * 
 
-def reply():
-    replies = ["Hey there! I would love an invite. I also tip 15%.", "May I come in? Thank you!! I will happily tip 15%!", "Please invite me if you get a chance! I tip well.", "Looking to sell, thanks.", "Hi! Would love to join!", "Interested in selling!!", "Heyyyy I have an inventory of turnips I need to get rid of! I will share my profits too.", "Thank you for doing this, let me know if I can get an invite.", "Would very much appreciate it if you let me sell."] 
-    return replies[random.randint(0, len(replies)-1)]
+eval(10, 200, 15, 30)
 
-reddit = praw.Reddit(client_id='PFyXXXXXXXXXX',
-                     client_secret='wIQgqpXXXXXXXXXXXXXXXXXXXXX',
-                     user_agent='windows:turnip (by u/caturian)',
-                     username='catXXXXX',
-                     password='KXXXXXXXX')
-
-state = False
-ids=[]
-while state == False:
-    for submission in reddit.subreddit('acturnips').new(limit=10):
-        timestamp = (time.time()-submission.created_utc)/60
-        if (timestamp < 15 and timestamp > 2) and "buying" in submission.title and submission.id not in ids and int(re.findall("\d+", submission.title)[0]) > 350:
-            submission.reply(reply())
-            ids.append(submission.id)
-            sleep(random.randint(6, 15))
-        else:
-            sleep(4)
-            
+# this function call tells the bot to do the following: look at the first 10 submissions in https://www.reddit.com/r/acturnips/new/. If a submission (1) advertises a buying price over 200 bells and (2) was posted less than 30 minutes ago, but more than 15 minutes ago then reply to the submission.
